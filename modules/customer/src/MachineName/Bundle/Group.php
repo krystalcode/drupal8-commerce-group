4858a75f621102c6a87b4ededfc50951a8d6899a
krystalcode <krystalcode@2392706.no-reply.drupal.org>
2020-07-20 00:50:26 +0000

    Issue #3160208 by krystalcode: Create Customer group type

---
+++ modules/customer/src/MachineName/Bundle/Group.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\gcommerce_customer\MachineName\Bundle;
+
+/**
+ * Class holding machine names for group types.
+ */
+class Group {
+
+  /**
+   * Holds the machine name for the Customer group type.
+   */
+  const CUSTOMER = 'gcommerce_customer';
+
+}
