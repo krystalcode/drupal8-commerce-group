5cde69fe1796be236385e2ecf8b9578399308c1f
krystalcode <krystalcode@2392706.no-reply.drupal.org>
2020-08-24 08:45:13 +0000

    Issue #3166879 by krystalcode: Configure whether order items can be split per type

---
+++ modules/split/src/MachineName/Field/OrderItem.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\gcommerce_split\MachineName\Field;
+
+/**
+ * Holds machine names of Order Item entity fields.
+ */
+class OrderItem {
+
+  /**
+   * Holds the order item's split items.
+   */
+  const SPLIT_ITEMS = 'gcommerce_split_items';
+
+}
